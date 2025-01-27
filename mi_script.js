1e8767cf661c3850bc6e310b43d78f4fa4f675d4
NICOLASSALMERON <jpermal075@g.educaand.es>
2025-01-27 20:34:45 +0100

    Update mi_script.js

--- mi_script.js
+++ mi_script.js
@@ -6,7 +6,16 @@
 	    var numero =  prompt("Introduce una OPCIÓN válida: ");
             menu Number (menu); // Convertimos el texto a numero
             }
-                     
+
+ 	    document.write ("<br>");
+            var numero =  prompt("Introduce un número entre 1 y 100:");  // Pedimos al usuario que introduzca un número
+            numero = Number(numero); // Convertimos el texto a numero
+
+            // Validamos que el número esté dentro del rango
+            while (numero < 1 || numero > 100) {
+            numero = prompt("Número inválido. Introduce un número entre 1 y 100:");
+            numero = Number (numero); // Convertimos el texto a numero
+            }
             //Iniciamos las variables
             var suma = 0; 
             var i = 1;
