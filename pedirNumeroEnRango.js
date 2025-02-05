9ad9642a6570502cd4ee0a5349cd50084e1dfc34
NICOLASSALMERON <jpermal075@g.educaand.es>
2025-02-05 22:09:17 +0100

    Update pedirNumeroEnRango.js

--- pedirNumeroEnRango.js
+++ pedirNumeroEnRango.js
@@ -1,7 +1,7 @@
 function pedirNumeroEnRango(min, max) {
   var numero;
   do {
-    numero = prompt("Ingrese un número entre:" + min +" y " + max + ":");
+    numero = prompt("Ingrese un número entre " + min +" y " + max + ":");
     numero = Number(numero); 
   } while (numero < min || numero > max);
 
