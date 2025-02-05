6cd326efb4ed1f4c7a8a7f32b61d0b1607adc9b3
NICOLASSALMERON <jpermal075@g.educaand.es>
2025-02-05 09:46:30 +0100

    Update script-leer-numeros.js

--- script-leer-numeros.js
+++ script-leer-numeros.js
@@ -1,8 +1,8 @@
-            var numero =  prompt("Introduce un número entre 1 y 100:");  // Pedimos al usuario que introduzca un número
+            var numero =  prompt("Introduce un número entre 1 y 10000:");  // Pedimos al usuario que introduzca un número
             numero = Number(numero); // Convertimos el texto a numero
 
             // Validamos que el número esté dentro del rango
-            while (numero < 1 || numero > 100) {
+            while (numero < 1 || numero > 10000) {
             numero = prompt("Número inválido. Introduce un número entre 1 y 100:");
             numero = Number (numero); // Convertimos el texto a numero
             }
