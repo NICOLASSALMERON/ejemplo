f7c8d5da32cdee088a6963b8eb74e27fbb5eb52d
NICOLASSALMERON <jpermal075@g.educaand.es>
2025-02-06 10:56:31 +0100

    Update comprobarSiEsPrimo.js

--- comprobarSiEsPrimo.js
+++ comprobarSiEsPrimo.js
@@ -1,5 +1,6 @@
             function comprobarEsPrimo(numero) {
                 var esPrimo;
+                var i;
                 
                 // Los números negativos, 0 y 1 no son primos
                 if (numero <= 1) {
@@ -7,7 +8,7 @@
                         return esPrime;
                       }
                 // Comprobamos si el número es divisible por algún número entre 2 y su raíz cuadrada.
-                for (var i = numero-1; i <= 2; i--) {
+                for (i = numero-1; i <= 2; i = i-1) {
                     if (numero % i === 0) {
                             esPrimo = false;
                             return esPrime;
