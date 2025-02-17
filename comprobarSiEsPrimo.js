f9634adfc34030c9d875e1856268ff162560c912
NICOLASSALMERON <jpermal075@g.educaand.es>
2025-02-17 12:51:09 +0100

    Update comprobarSiEsPrimo.js

--- comprobarSiEsPrimo.js
+++ comprobarSiEsPrimo.js
@@ -5,7 +5,7 @@ function comprobarEsPrimo (numero)
 
   for (i=numero-1; i>=2; i=i-1 )
     {
-      if ( numero % i == 0 )
+      if ( numero % i === 0 )
         {
         esPrimo = false;  
         }
